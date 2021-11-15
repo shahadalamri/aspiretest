5374a6dec0d79544369eabf1811d6fbc6772be10
shahadalamri <shahadalamri.v@gmail.com>
2021-11-15 13:43:24 +0300

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -40,11 +40,11 @@ export const Header = (props) => {
                     >
                         <Grid item xs={8} sm={8} md={8} lg={8}>
                               <motion.div className="intro-text" transition={{ opacity: { duration: 3 },translateY:{ duration:3  } }} animate={{ opacity: shouldShowActions ? 1 : 0,translateY:"0px" }} initial={{ opacity:0,translateY:"-150px"  }}>
-                                <img alt="Header" className="imageHeader" src={"./img/AspireFinalLOGO/png/AspireLogofinal619.svg"} width="55%" height="auto"/>
-                                <h1 style={{ fontSize:"4.6vw",color:"#fff",margin:"0" }}>
+                                {/* <img alt="Header" className="imageHeader" src={"./img/AspireFinalLOGO/png/AspireLogofinal619.svg"} width="55%" height="auto"/> */}
+                                {/* <h1 style={{ fontSize:"4.6vw",color:"#fff",margin:"0" }}>
                                   {props.data ? props.data.title : 'Loading'}
-                                </h1>
-                                <p style={{ fontSize:"1.5vw",color:"#fff" }}>(an <a href="https://exeder.com/" target="_blank" rel="noopener noreferrer" ><span  style={{ color:"#44849c",fontWeight:'bold' }}>EXEDER</span></a> company)</p>
+                                </h1> */}
+                                {/* <p style={{ fontSize:"1.5vw",color:"#fff" }}>(an <a href="https://exeder.com/" target="_blank" rel="noopener noreferrer" ><span  style={{ color:"#44849c",fontWeight:'bold' }}>EXEDER</span></a> company)</p> */}
                                   <FormDialog /> 
                                   </motion.div>
                           </Grid>
@@ -58,7 +58,7 @@ export const Header = (props) => {
 
                    </Grid>
                    
-              <video id="background-video" style={{ borderRadius:"5px" }} src="/img/AspireFinalLOGO/png/video1.mp4" loop autoPlay muted playsInline>
+              <video id="background-video" style={{ borderRadius:"5px" }} src="/img/AspireFinalLOGO/png/slideshow video website.mp4" loop autoPlay muted playsInline>
               </video>
            </div>
       //  </div>
